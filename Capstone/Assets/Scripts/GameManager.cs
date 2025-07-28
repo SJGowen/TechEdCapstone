1758910a8af7697e2506a515473c6ae99e9d0a62
Stephen <sjgowen@gmail.com>
2025-07-28 11:39:02 +0100

    Basic Welcome Screen added
    Containing buttons Start Game, Audio Settings and Quit

---
+++ Capstone/Assets/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] Button playButton;
+    [SerializeField] Button settingsButton;
+    [SerializeField] Button quitButton;
+
+    void Start()
+    {
+        playButton.onClick.AddListener(PlayGame);
+        settingsButton.onClick.AddListener(AudioSettings);
+        quitButton.onClick.AddListener(QuitGame);
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void PlayGame()
+    {
+        Debug.Log("Play game button clicked");
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void AudioSettings()
+    {
+        Debug.Log("Audio settings button clicked");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit game button clicked");
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_WEBGL   
+        Application.OpenURL("https://softmine.itch.io/");
+#else
+        Application.Quit();
+#endif
+    }
+}
