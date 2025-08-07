be94db568d0a5000c1a77994ccd290ff8adb1978
Imotep92 <Imogyn.nicholas@yahoo.co.uk>
2025-08-07 10:53:06 +0100

    Enemy_bullet_Ai
    both the droid and lgm enemy AI's work and interact with the player character

--- Capstone/Assets/Scripts/Enemy_Scripts/EnemyLaserScript.cs
+++ Capstone/Assets/Scripts/Enemy_Scripts/EnemyLaserScript.cs
@@ -2,40 +2,48 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public class EnemyLaserScript : MonoBehaviour
+public class EnemyLaserScript : EnemyAI
 {
-    [SerializeField] private GameObject player;
+
     [SerializeField] Rigidbody2D _lgmLaser;
     [SerializeField] public float _laserForce;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        _lgmLaser = GetComponent<Rigidbody2D>(); // laser bullet
-        player = GameObject.FindGameObjectWithTag("Player"); // Find Player
 
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+    public override void Start()
+    {
+
+        _lgmLaser = GetComponent<Rigidbody2D>(); // laser bullet
+        _lgmLaser.gravityScale = 0f; // Applies gravity scale to this Rigidbody
 
 
         Vector3 direction = player.transform.position - transform.position;
-
         _lgmLaser.linearVelocity = new Vector2(direction.x, direction.y).normalized * _laserForce;
 
         float _lgmLaserRot = Mathf.Atan2(-direction.y, direction.x) * Mathf.Rad2Deg;
-
         transform.rotation = Quaternion.Euler(0, 0, _lgmLaserRot + 90);
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    // protected override void Update()
+    // {
 
-    }
-    
+    // }
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            collision.gameObject.GetComponent<PlayerController>().health -= 1;
+            Attack();
             Destroy(gameObject);
         }
+    }
+
+    protected override void Attack()
+    {
+        base.Attack();
     }
 }
