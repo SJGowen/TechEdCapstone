32c9dbbc5506b8459d2f810001b526450fbf9360
ApolloGOM <ApolloGOM365@gmail.com>
2025-08-07 14:02:19 +0100

    Finishing the boss scripts and adding the boss to the boss stage

--- Capstone/Assets/Scripts/BossRun.cs
+++ Capstone/Assets/Scripts/BossRun.cs
@@ -3,24 +3,37 @@ using UnityEngine;
 public class BossRun : StateMachineBehaviour
 {
     public float speed = 2.5f;
+    public float attackRange = 3f;
+
     Transform player;
     Rigidbody2D rb;
- override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    BossFight boss;
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        //player = GameObject.FindGameObjectsWithTag("Player").transform;
+       player = GameObject.FindGameObjectWithTag("Player").transform;
        rb = animator.GetComponent<Rigidbody2D>();
     }
 
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        boss.LookAtPlayer();
+
         Vector2 target = new Vector2(player.position.x, rb.position.y);
-        Vector2.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
-        //rb.MovePosition(newPos);
+        Vector2 newPos = Vector2.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
+        rb.MovePosition(newPos);
+
+
+        if (Vector2.Distance(player.position, rb.position) <= attackRange)
+        {
+            //Attack the player
+            animator.SetTrigger("Attack");
+        }
     }
 
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-
+        animator.ResetTrigger("Attack");
     }
 }
 
